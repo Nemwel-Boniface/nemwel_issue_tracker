4e78f29ee6642cdcffd22ef8e904fd44f3e1a976
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-05-27 15:42:29 +0300

    Update implemented custom slugs for the projects using friendlyID for:   - SEO purposes of the app   - Run the migrations to update the schema file

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,15 @@
 class Project < ApplicationRecord
+  # FriendlyID for custom slugs
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
   # Associations
   belongs_to :user
 
   # Validations
   validates :title, :description, presence: true, length: { minimum: 5 }
+
+  def to_param
+    slug
+  end
 end
